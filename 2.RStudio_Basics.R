53f5fc4949d30f67dfbc241cf104085caa5f227e
sdjangam <sdjangam71@gmail.com>
2021-03-28 14:13:23 -0700

    Model Added

--- 2.RStudio_Basics.R
+++ 2.RStudio_Basics.R
@@ -1,12 +1,7 @@
-#Cleanup environment
-rm(list = ls())
-# free memory
-gc()
-setwd("C:/Users/gmanish/Downloads/openminds/code/R/")
-
-#R works like a calculator
 1 / 200 * 30
+
 (59 + 73 + 2) / 3
+
 sin(pi / 2)
 
 #Assignment in R
@@ -17,6 +12,7 @@ this_is_a_really_long_name <- 2.5
 r_rocks <- 2 ^ 3
 r_rock
 R_rocks
+
 
 #Calling functions
 seq(1, 10)
@@ -30,12 +26,3 @@ y <- seq(1, 10, length.out = 5)
 y
 
 (y <- seq(1, 10, length.out = 5))
-
-
-#Projects
-
-library(tidyverse)
-ggplot(diamonds, aes(carat, price)) +
-geom_hex()
-ggsave("diamonds.pdf")
-write_csv(diamonds, "diamonds.csv")
